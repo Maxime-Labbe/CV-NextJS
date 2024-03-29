91f92040717a7f6782764cd1ee0bd047d0f2bb36
Maxime LABBE <Maxime.LABBE@enigma-school.com>
2024-03-29 17:00:05 +0100

    Fixing compatibility bugs

--- app/card.tsx
+++ app/card.tsx
@@ -13,14 +13,14 @@ export default function Card({
 }) {
     const titleHTML = []
     if (image) {
-        titleHTML.push(<a href={link} target="_blank" className="mx-auto mt-6"><Image className="hidden sm:block absolute top-32 left-24" src={image} alt="" width={100} height={100} key={image} /><Image className="sm:hidden block absolute top-28 left-11" src={image} alt="" width={80} height={80} key={image} /></a>)
+        titleHTML.push(<a href={link} target="_blank" className="mx-auto mt-6"><Image className="hidden sm:block absolute top-32 left-24" src={image} alt="" width={100} height={100} key={image} /><Image className="sm:hidden block fixed top-28 left-11" src={image} alt="" width={80} height={80} key={image} /></a>)
     } else {
-        titleHTML.push(<h2 className="text-center mx-auto mt-[115px] text-4xl font-bold text-[#EEEDFF] group-hover:text-transparent" key={title}>{title}</h2>)
+        titleHTML.push(<h2 className="hidden sm:block text-center mx-auto mt-[115px] text-4xl font-bold text-[#EEEDFF] group-hover:text-transparent" key={title}>{title}</h2>)
     }
     return (
         <div className="card group">
-            <h2 className="text-center mx-auto mt-[10px] text-4xl font-bold text-transparent group-hover:text-[#EEEDFF]">{title}</h2>
-            <p className="w-11/12 text-center absolute left-3 mt-6 text-xl font-normal text-transparent group-hover:text-[#EEEDFF]">{description}</p>
+            <h2 className="text-center mx-auto mt-[10px] text-[25px] sm:text-4xl font-bold sm:text-transparent sm:group-hover:text-[#EEEDFF]">{title}</h2>
+            <p className="w-11/12 text-center absolute left-3 mt-4 sm:mt-2 text-base sm:text-xl font-normal sm:text-transparent sm:group-hover:text-[#EEEDFF]">{description}</p>
             {titleHTML}
         </div>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex flex-col items-center sm:flex-row content-around w-full">
-      <div className="mt-6 w-4/5 sm:w-3/5 flex flex-col sm:justify-end h-auto sm:h-72 gap-0 text-center">
-        <h1 className="text-3xl font-medium text-center">Hey ! I'm <span className="text-[#F9F871] font-semibold text-4xl">Maxime LABBE.</span></h1>
+      <div className="mt-16 w-4/5 sm:w-3/5 flex flex-col sm:justify-end h-auto sm:h-72 gap-0 text-center">
+        <h1 className="text-3xl font-medium sm:text-end">Hey ! I'm <span className="text-[#F9F871] font-semibold text-4xl">Maxime LABBE.</span></h1>
         <h2 className="text-2xl font-medium sm:text-end">Je suis étudiant informatique à <br />ENIGMA-SCHOOL à Lille.</h2>
       </div>
       <div className="w-2/5 flex justify-start items-end h-72">
