f8cd98cbe28b0a2760c646dfcb24ced1f7e794b6
Maxime LABBE <Maxime.LABBE@enigma-school.com>
2024-04-10 10:50:56 +0200

    Bugs fixing

--- app/aboutme/page.tsx
+++ app/aboutme/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { JSXElementConstructor, Key, useEffect, useState } from 'react';
+import { useEffect, useState, useRef, useLayoutEffect } from 'react';
 import Card from '../card';
 import localFont from "next/font/local";
 import { notFound } from 'next/navigation';
@@ -180,7 +180,7 @@ export default function AboutMe() {
                     ))}
                 </div>
             </div>
-            <div className='sm:block hidden bg-white mx-auto mt-4 xl:w-28 w-24 h-10 rounded-full lg:absolute xl:right-16 lg:right-0 lg:bottom-12 cursor-pointer' onClick={() => window.scrollTo(0, window.innerHeight - 50)}>
+            <div className='sm:block hidden bg-[#EEEDFF] mx-auto mt-4 xl:w-28 w-24 h-10 rounded-full lg:absolute xl:right-16 lg:right-0 lg:bottom-12 cursor-pointer' onClick={() => window.scrollTo(0, window.innerHeight - 40)}>
                 <svg className='relative left-2 xl:top-2.5 top-3 rotate-180 xl:h-5 h-4' viewBox="0 0 512 512">
                     <path d="M233.4 105.4c12.5-12.5 32.8-12.5 45.3 0l192 192c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L256 173.3 86.6 342.6c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3l192-192z"></path>
                 </svg>
--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,31 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center sm:flex-row content-around w-full">
       <div className="mt-16 w-4/5 sm:w-3/5 flex flex-col sm:justify-end h-auto sm:h-72 gap-0 text-center">
-        <h1 className="text-3xl font-medium mb-2 sm:text-end sm:mb-0">Hey ! I'm <span className="text-[#F9F871] font-semibold text-4xl">Maxime LABBE.</span></h1>
+        <h1 className="text-3xl font-medium mb-2 sm:text-end sm:mb-0">Hey, je suis <span className="text-[#F9F871] font-semibold text-4xl">Maxime LABBE.</span></h1>
         <h2 className="text-2xl font-medium sm:text-end">Étudiant en informatique à <br />ENIGMA-SCHOOL à Lille.</h2>
       </div>
-      <div className="w-2/5 flex justify-start items-end h-72">
-        <img src="" alt="" />
+      <div className="sm:w-2/5 w-fukk flex justify-center items-end content-end h-72 lg:mb-[-300px] sm:mb-[-200px]">
+        <Image
+          src="/testimage.jpg"
+          alt="Photo de Maxime LABBE"
+          width={350}
+          height={350}
+          className="rounded-full lg:block hidden"
+        />
+        <Image
+          src="/testimage.jpg"
+          alt="Photo de Maxime LABBE"
+          width={250}
+          height={250}
+          className="rounded-full lg:hidden sm:block hidden"
+        />
+        <Image
+          src="/testimage.jpg"
+          alt="Photo de Maxime LABBE"
+          width={250}
+          height={250}
+          className="rounded-lg border-2 border-white block sm:hidden"
+        />
       </div>
     </main>
   );
